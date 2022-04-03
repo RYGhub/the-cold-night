edc036941f5c94d13c3c5c5324b1dc05363ee71b
Stefano Pigozzi <me@steffo.eu>
2022-04-03 08:47:39 +0200

    :wrench: Add utility static methods to `Ownership`

--- src/entities/behaviours/Ownership.gd
+++ src/entities/behaviours/Ownership.gd
@@ -1,4 +1,13 @@
 extends Node
+class_name Ownership
 
 
 var entity_owner: Node = null
+
+
+static func get_value(first) -> Node:
+	var first_node = first.get_node("Ownership")
+	var first_alliance = null
+	if first_node != null:
+		first_alliance = first_node.alliance
+	return first_alliance
