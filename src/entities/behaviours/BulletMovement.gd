c8f0eda16fc16b014643710ecc37593a62d1ed36
Stefano Pigozzi <me@steffo.eu>
2022-04-03 08:50:02 +0200

    :bug: Fix `BulletMovement` not having the delta applied anymore

--- src/entities/behaviours/BulletMovement.gd
+++ src/entities/behaviours/BulletMovement.gd
@@ -7,8 +7,8 @@ export var movement_per_second: float = 400.0
 signal hit(collision)
 
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	var direction = Vector2(cos(target.rotation), sin(target.rotation))
-	var collision: KinematicCollision2D = target.move_and_collide(direction * movement_per_second)
+	var collision: KinematicCollision2D = target.move_and_collide(direction * movement_per_second * delta)
 	if collision != null:
 		emit_signal("hit", collision)
