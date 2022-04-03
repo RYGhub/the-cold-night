948405ddd59216469a739f8fa2b3b14aea043b8c
Cookie-CHR <pacchetto99@gmail.com>
2022-04-03 10:23:32 +0200

    ⚙️ Created enemy spawner

--- src/entities/behaviours/AttractedTo.gd
+++ src/entities/behaviours/AttractedTo.gd
@@ -2,7 +2,7 @@ extends Node
 
 
 export var movement_per_second: float = 32.0
-export var goal_path: String = "../../Fire"
+export var goal_path: String = "../../PhaseOneContainer/Fire"
 
 
 onready var target: KinematicBody2D = get_parent()
@@ -14,14 +14,14 @@ signal touching_goal
 var goal_reached_triggered: bool = false
 signal goal_reached
 
-
 func _physics_process(_delta):
-	var direction: Vector2 = (goal.global_position - target.global_position).normalized()
-	var _motion: Vector2 = target.move_and_slide(direction * movement_per_second, Vector2.ZERO)
-	for slide_no in target.get_slide_count():
-		var slide = target.get_slide_collision(slide_no)
-		if slide.collider == goal:
-			emit_signal("touching_goal")
-			if not goal_reached_triggered:
-				emit_signal("goal_reached")
-				goal_reached_triggered = true
+	if target.global_position != null:
+		var direction: Vector2 = (goal.global_position - target.global_position).normalized()
+		var _motion: Vector2 = target.move_and_slide(direction * movement_per_second, Vector2.ZERO)
+		for slide_no in target.get_slide_count():
+			var slide = target.get_slide_collision(slide_no)
+			if slide.collider == goal:
+				emit_signal("touching_goal")
+				if not goal_reached_triggered:
+					emit_signal("goal_reached")
+					goal_reached_triggered = true
