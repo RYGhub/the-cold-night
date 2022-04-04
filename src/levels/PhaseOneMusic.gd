cdb57f90e6090afb54145a297f2c82ba29e8b51b
Stefano Pigozzi <me@steffo.eu>
2022-04-04 15:02:36 +0200

    :wrench: Add functionality to the mute button

--- src/levels/PhaseOneMusic.gd
+++ src/levels/PhaseOneMusic.gd
@@ -7,14 +7,26 @@ export var bell_min: float = 0.20
 export var bell_max: float = 0.60
 export var drum_min: float = 0.40
 export var drum_max: float = 0.80
+export var muted: bool = false setget set_mute
 
 
 func _on_Fire_intensity_changed(value):
 	$Choir.volume_db = (smoothstep(choir_min, choir_max, value) - 1) * 60
-	$Choir.bus = "Master" if $Choir.volume_db > -60 else "Mute"
+	$Choir.bus = "Master" if not muted and $Choir.volume_db > -60 else "Mute"
 
 	$Bell.volume_db = (smoothstep(bell_min, bell_max, value) - 1) * 60
-	$Bell.bus = "Master" if $Choir.volume_db > -60 else "Mute"
+	$Bell.bus = "Master" if not muted and $Bell.volume_db > -60 else "Mute"
 
 	$Drum.volume_db = (smoothstep(drum_min, drum_max, value) - 1) * 60
-	$Drum.bus = "Master" if $Choir.volume_db > -60 else "Mute"
+	$Drum.bus = "Master" if not muted and  $Drum.volume_db > -60 else "Mute"
+
+
+func set_mute(value):
+	muted = value
+	$Choir.bus = "Master" if not muted and $Choir.volume_db > -60 else "Mute"
+	$Bell.bus = "Master" if not muted and $Bell.volume_db > -60 else "Mute"
+	$Drum.bus = "Master" if not muted and  $Drum.volume_db > -60 else "Mute"
+
+
+func _on_MuteButton_pressed():
+	set_mute(not muted)
